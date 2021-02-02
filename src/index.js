a7d57faca273a8dabafc1ab1c449ca9854260a73
hüseyin <akkaya.hu@gmail.com>
2021-02-02 12:07:10 +0300

    add redux thunk settings

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import 'antd/dist/antd.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './store/store'
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
